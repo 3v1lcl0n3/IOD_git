c0da2fa8a7164d08c8184d6bd730c88260246920
P5Tech <37521629+P5Tech@users.noreply.github.com>
2018-06-02 16:32:35 +0200

    Dodan test 3

--- TestClass.java
+++ TestClass.java
@@ -31,4 +31,23 @@ public class TestClass {
 		}
 	}
 	
+	@Test
+	public void test3() {
+		obrada testObrada = new obrada();
+		File file1 = new File("R2_0test.arff");
+		File file2 = new File("R2_1test.arff");
+		String ocekivani =
+				"Broj modula u prvom datasetu: 10\n" + 
+				"Broj modula u drugom datasetu: 10\n" + 
+				"Broj promjenjenih modula: 7\n" + 
+				"Broj nepromijenjenih modula: 1\n" + 
+				"Broj modula koji su maknuti u novoj verziji: 2\n" + 
+				"\n" + 
+				"Broj FP kandidata(promjenjeni + maknuti): 9\n" + 
+				"Broj NFP kandidata(nepromjenjeni moduli): 1\n" + 
+				"";
+		
+		assertEquals(ocekivani, testObrada.obradi(file1, file2, 'n'));
+	}
+	
 }
